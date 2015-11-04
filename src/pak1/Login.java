0ceb0dc7745aaf474cba78f77548b4dfec644f20
SJobs <stevejobscbe42@gmail.com>
2015-11-04 13:48:25 +0530

    04-Nov-2015 1

--- src/pak1/Login.java
+++ src/pak1/Login.java
@@ -2,6 +2,7 @@ package pak1;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Properties;
 
 import org.openqa.selenium.By;
@@ -9,7 +10,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
 
 
 public class Login 
@@ -25,6 +25,7 @@ public class Login
 	@Test
 	  public Properties Attributes() throws IOException
 	  {
+		System.out.println("Prop Attributes");
 		FileReader ff = new FileReader("D:/test/0_KeyedTestNG/src/pak2/KeyedIN_Attributes");
 		Properties pp = new Properties();
 		pp.load(ff);
@@ -34,19 +35,21 @@ public class Login
 	@Test
 	  public Properties Testdata() throws IOException 
 	  {
+		System.out.println("Prop Testdata");
 		FileReader ff = new FileReader("D:/test/0_KeyedTestNG/src/pak2/KeyedIN_TestData");
 		Properties pp = new Properties();
 		pp.load(ff);
 		return(pp);  
 	  }
 	
-	@Test (priority=1)
+	@Test (priority=0)
 	  public void Login1() throws IOException 
 	  {
 		try{
-			dr.findElement(By.xpath(Attributes().getProperty("UN"))).sendKeys(Testdata().getProperty("UN"));
-			dr.findElement(By.xpath(Attributes().getProperty("Pwd"))).sendKeys(Testdata().getProperty("Pwd"));
-			dr.findElement(By.xpath(Attributes().getProperty("Click"))).click();
+			System.out.println("Start Login");
+			dr.findElement(By.xpath("/html/body/div/div/form/ul/li[1]/input")).sendKeys("lizc-admin");
+			dr.findElement(By.xpath("/html/body/div/div/form/ul/li[2]/input")).sendKeys("password");
+			dr.findElement(By.xpath("/html/body/div/div/form/ul/li[3]/input")).click();
 			String Str1 = dr.getPageSource();
 			String Str2 = "User Name";
 			if(Str1.contains(Str2))
@@ -57,8 +60,7 @@ public class Login
 			{
 				System.out.println("Logged In Successfully");
 			}	
-			//dr.findElement(By.xpath(Attributes().getProperty("Home"))).click();
-			}catch(NullPointerException e){
+			}catch(NoSuchElementException e){
 				System.out.println(e);
 				System.out.println("rest of the code...");
 			}
--- src/pak1/SOManipulation.java
+++ src/pak1/SOManipulation.java
@@ -2,11 +2,15 @@ package pak1;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
@@ -17,11 +21,38 @@ public class SOManipulation extends Login
 {
 	public static String Str;
 	
-	@Test (priority=2)
+	@BeforeTest
+	  public void Login() throws IOException
+	  {
+		try{
+			dr.manage().window().maximize();
+			dr.get("http://kimdev01.keyedinuat.com/Dev03/Tab/71");
+			WebDriverWait wait = new WebDriverWait(dr, 30);
+			wait.until(ExpectedConditions.textToBePresentInElement(By.xpath("/html/body/div/div/form/ul/li[1]/label"), "User Name"));
+			System.out.println("Explicit Wait");
+			dr.findElement(By.xpath("/html/body/div/div/form/ul/li[1]/input")).sendKeys("lizc-admin");
+			dr.findElement(By.xpath("/html/body/div/div/form/ul/li[2]/input")).sendKeys("password");
+			dr.findElement(By.xpath("/html/body/div/div/form/ul/li[3]/input")).click();
+			String Str1 = dr.getPageSource();
+			String Str2 = "User Name";
+			if(Str1.contains(Str2))
+			{
+				System.out.println("Yet To Login");
+			}
+			else
+			{
+				System.out.println("Logged In Successfully");
+			}	
+			}catch(NoSuchElementException e){
+				System.out.println(e);
+				System.out.println("rest of the code...");
+			}
+	  }
+	
+	@Test (priority=1)
 	  public void AddSO() 
 	  {
 		System.out.println("************** SALES ORDER CREATION **************");
-		//dr.findElement(By.xpath(".//span[@class='k-icon k-icon-clipboard']")).click();
 		dr.findElement(By.xpath(".//a[@href='/Dev03/Form/Create/70']")).click();
 		dr.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		dr.findElement(By.xpath("html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div/fieldset[1]/div/div/div/div[1]/div[2]/div/div/div[2]/div/div[1]/span/div/a/span[1]")).click();
@@ -39,16 +70,14 @@ public class SOManipulation extends Login
 		dr.findElement(By.xpath(".//button[@class='btn btn-xs btn-success']")).click();
 		WebElement WE1 = dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div/fieldset[1]/div/div/div/div[1]/div[1]/div/div/div[2]/div/div[1]/span"));
 		Str = WE1.getText();
-		System.out.println("New Sales Order Created Succesfully & Order Number is: "+Str);
-		  
+		System.out.println("New Sales Order Created Succesfully & Order Number is: "+Str);	  
 	  }
 	
-	@Test (priority=3)
+	@Test (priority=2)
 	  public void EditSO() throws IOException
 	  {
-		System.out.println("Created Order Number is : "+Str);
 		System.out.println("************** SALES ORDER SEARCH **************");
-		dr.get(Attributes().getProperty("URL"));
+		dr.get("http://kimdev01.keyedinuat.com/Dev03/Tab/71");
 		dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div[2]/div[2]/ul/li[1]/input[1]")).click();
 		dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div[2]/div[2]/ul/li[1]/input[1]")).sendKeys(Str);
 		dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div[2]/div[2]/ul/li[9]/button[1]")).click();
@@ -57,19 +86,19 @@ public class SOManipulation extends Login
 		int C2 = V2.size();
 			if(C2==0)
 			{
-				System.out.println("if : Entered Order Number "+ Str +" is not filtered in the below list");
+				System.out.println("Entered Order Number "+ Str +" is not filtered in the below list");
 			}
 				else
 				{
-					System.out.println("Else : Entered Order Number "+ Str +" is fi ltered in the below list");
+					System.out.println("Entered Order Number "+ Str +" is filtered in the below list");
 				}
 	  }
 	
-	@Test (priority=4)
+	@Test (priority=3)
 	  public void ViewSO() throws IOException
 	  {
 		System.out.println("************** SALES ORDER VIEW **************");
-		dr.get(Attributes().getProperty("URL"));
+		dr.get("http://kimdev01.keyedinuat.com/Dev03/Tab/71");
 		String x1 = "/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div[2]/div[3]/div[2]/div/table/tbody/tr[";
 		String x2 = "]/td[1]/div[2]/a[6]";
 		String x3 = "/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div[2]/div[3]/div[2]/div/table/tbody/tr[";
@@ -79,18 +108,18 @@ public class SOManipulation extends Login
 		int TableSize = allRows.size()-1;
 		Random rand = new Random(System.currentTimeMillis());
 		int rval = rand.nextInt(TableSize);
-		System.out.println("mber: "+rval);
+		System.out.println("Row number: "+rval);
 		dr.findElement(By.xpath(x1+rval+x2)).click();
 		dr.findElement(By.xpath(x3+rval+x4)).click();		
 		WebElement WW2 = dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div/fieldset[1]/div/div/div/div[1]/div[1]/div/div/div[2]/div/div[1]/span"));
-		System.out.println("Order Number is: "+WW2.getText());
+		System.out.println("Viewed Order Number is: "+WW2.getText());
 	  }
 	
-	@Test (priority=5)
+	@Test (enabled=false)
 	  public void DeleteSO() throws IOException
 	  {
 		System.out.println("************** SALES ORDER DELETE **************");
-		dr.get(Attributes().getProperty("URL"));
+		dr.get("http://kimdev01.keyedinuat.com/Dev03/Tab/71");
 		//Random Order Navigate
 		String x1 = "/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div[2]/div[3]/div[2]/div/table/tbody/tr[";
 		String x2 = "]/td[1]/div[2]/a[6]";
@@ -102,22 +131,20 @@ public class SOManipulation extends Login
 		String x6 = "]/td[3]/div/a";
 		WebElement table = dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div[2]/div[3]/div[2]/div/table"));
 		List<WebElement> allRows = table.findElements(By.tagName("tr"));
-		int TableSize = allRows.size()-1;
+		int TableSize = allRows.size();
 		Random rand = new Random(System.currentTimeMillis());
 		int rval = rand.nextInt(TableSize);
 		System.out.println("Row Number: "+rval);
 		dr.findElement(By.xpath(x1+rval+x2)).click();
 		String S1 = dr.findElement(By.xpath(x5+rval+x6)).getText();
-		System.out.println("Order Number to be Delete : "+S1);
+		System.out.println("Deleted Order Number is: "+S1);
 		dr.findElement(By.xpath(x3+rval+x4)).click();	
 		dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[1]/button[1]")).click();
 	  }
 	
-	@Test (priority=6)
-	  public void T4()
-	  {
-		  dr.findElement(By.xpath("/html/body/div")).click();
-		  System.out.println("***************************************@Testcase Skipped ***************************************");
-		  throw new SkipException("Test");
-	  }
+	@AfterTest
+	   public void Quit()
+	   {
+		dr.quit();
+	   }
 }
