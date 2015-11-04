e8fd51c7058b7c6f805bdef974b23500d328944e
SJobs <stevejobscbe42@gmail.com>
2015-11-04 16:33:04 +0530

    04-Nov-2015 Demo1

--- src/pak1/SOManipulation.java
+++ src/pak1/SOManipulation.java
@@ -21,7 +21,7 @@ public class SOManipulation extends Login
 {
 	public static String Str;
 	
-	@BeforeTest
+	@Test(enabled=true)
 	  public void Login() throws IOException
 	  {
 		try{
@@ -74,7 +74,67 @@ public class SOManipulation extends Login
 	  }
 	
 	@Test (priority=2)
-	  public void EditSO() throws IOException
+	public void EditSO()
+	{
+		dr.get("http://kimdev01.keyedinuat.com/Dev03/Tab/71");
+		String x1 = "/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div[2]/div[3]/div[2]/div/table/tbody/tr[";
+		String x2 = "]/td[1]/div[2]/a[2]";
+		
+		WebElement table = dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div[2]/div[3]/div[2]/div/table"));
+		List<WebElement> allRows = table.findElements(By.tagName("tr"));
+		int TableSize = allRows.size()-1;
+		Random rand = new Random(System.currentTimeMillis());
+		int rval = rand.nextInt(TableSize);
+		System.out.println("R Order: "+rval);
+			
+		dr.findElement(By.xpath(x1+rval+x2)).click();
+		
+/*		//Customer Selection
+		dr.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div/fieldset[1]/div/div/div/div[1]/div[2]/div/div/div[2]/div/div[1]/span/div/a/span[1]")).click();
+		String x3 = "/html/body/div[5]/ul/li[";
+		String x4 = "]";
+		
+		WebElement WE1 = dr.findElement(By.xpath("/html/body/div[5]/ul"));
+		List<WebElement> WB2 = WE1.findElements(By.xpath("/html/body/div[5]/ul/li"));
+		int DropdownVal1 = WB2.size();
+		Random r1 = new Random(System.currentTimeMillis());
+		int Drop = r1.nextInt(DropdownVal1)+1;
+		System.out.println("R DropDown: "+Drop);
+		dr.findElement(By.xpath(x3+Drop+x4)).click();
+		
+		//Status
+		dr.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[2]/div/div/fieldset[1]/div/div/div/div[4]/div[1]/div/div/div[2]/div/div[1]/span/div/a/span[1]")).click();
+		String x5 = "/html/body/div[5]/ul/li[";
+		String x6 = "]";
+		
+		WebElement WE2 = dr.findElement(By.xpath("/html/body/div[5]/ul"));
+		List<WebElement> WB3 = WE2.findElements(By.xpath("/html/body/div[5]/ul/li"));
+		int DropdownVal2 = WB3.size();
+		Random r2 = new Random(System.currentTimeMillis());
+		int Drop1 = r2.nextInt(DropdownVal2)+1;
+		System.out.println("R DropDown: "+Drop1);
+		dr.findElement(By.xpath(x5+Drop1+x6)).click();	*/
+				
+		//Save
+		dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[3]/div/button[1]")).click();
+		String S1 = "Some of the items in this list are not valid";
+		String S2 = dr.getPageSource();
+		
+		if(S2.contains(S1))
+		{
+			System.out.println("Unable to Update Sales Order due to invalid Sales Order Items");
+		}
+		else
+		{	
+			WebElement WB1 = dr.findElement(By.xpath("/html/body/div[2]/div/div[1]/div[1]/div/form/div[1]/label"));
+			String OrderNum = WB1.getText();
+			System.out.println(OrderNum+" is updated successfully");
+		}
+	}
+	@Test 
+	  public void SearchSO() throws IOException
 	  {
 		System.out.println("************** SALES ORDER SEARCH **************");
 		dr.get("http://kimdev01.keyedinuat.com/Dev03/Tab/71");
@@ -94,7 +154,7 @@ public class SOManipulation extends Login
 				}
 	  }
 	
-	@Test (priority=3)
+	@Test 
 	  public void ViewSO() throws IOException
 	  {
 		System.out.println("************** SALES ORDER VIEW **************");
@@ -115,7 +175,7 @@ public class SOManipulation extends Login
 		System.out.println("Viewed Order Number is: "+WW2.getText());
 	  }
 	
-	@Test (enabled=false)
+	@Test 
 	  public void DeleteSO() throws IOException
 	  {
 		System.out.println("************** SALES ORDER DELETE **************");
@@ -145,6 +205,6 @@ public class SOManipulation extends Login
 	@AfterTest
 	   public void Quit()
 	   {
-		dr.quit();
+		//dr.quit();
 	   }
 }
